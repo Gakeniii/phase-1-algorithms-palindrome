664e6254fcddaa2411b422220fbc38936f3abadd
Sifa Gakeni Muriithi <gakenisifa@gmail.com>
2024-10-20 20:33:53 +0300

    alogrithms

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  return word === word.split('').reverse().join('');
+
 }
 
 /* 
   Add your pseudocode here
 */
+// 1.Reverse the string.
+// 2.Compare the reversed string with the original string.
+// 3.If they are the same, return true; otherwise, return false.
 
-/*
-  Add written explanation of your solution here
-*/
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
